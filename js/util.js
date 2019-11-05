e76c01edc7b5fa1cfdcd5fe4adf8b79030013eb0
LPJworkroom <lpjworkroom@gmail.com>
2019-11-05 10:31:41 +0800

    。

---
+++ js/util.js
@@ -0,0 +1,16 @@
+function get(id){
+	return document.getElementById(id);
+}
+
+function geturlpara(nowurl,paraname){
+	if (nowurl.indexOf(paraname)==-1)	return "";
+	var begin=nowurl.indexOf(paraname)+paraname.length+1;
+	var end=nowurl.indexOf("&",begin);
+	/*最后一个参数*/
+	if (end==-1)	end=nowurl.length;
+	return nowurl.substr(begin,end-begin);
+}
+
+function log(str){
+	console.log(str);
+}
