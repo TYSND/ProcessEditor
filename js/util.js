abba245fcafa5c48f26405c887c2eed53310204c
LPJworkroom <lpjworkroom@gmail.com>
2019-11-09 10:21:37 +0800

    update create process

--- js/util.js
+++ js/util.js
@@ -2,6 +2,22 @@ function get(id){
 	return document.getElementById(id);
 }
 
+function openSubWin(innerHtml,beforeUnloadFunc){
+	var subWin=document.createElement("div");
+	subWin.style="	width:"+window.outerWidth+"px;height:"+window.outerHeight+"px;\
+					position:fixed;background:rgba(15, 19, 66, 0.55);top:0;left:0;";
+	subWin.innerHTML="<div class='bar' style='height:auto;width:30%;margin-left:auto;margin-right:auto;'>"+innerHtml+
+					"<div><button class='submitBut' id='subWinBut'>提交</button></div>"+"</div>";
+	document.body.appendChild(subWin);
+	subWin.childNodes[0].style.marginTop=(window.outerHeight-subWin.childNodes[0].offsetHeight*2)/2+"px";
+	//log((window.innerHeight-subWin.offsetHeight)/2);
+	//subWin.childNodes[0].style.marginTop="300px";
+	get("subWinBut").onclick=function(){
+		beforeUnloadFunc();
+		document.body.removeChild(subWin);
+	};
+}
+
 function geturlpara(nowurl,paraname){
 	if (nowurl.indexOf(paraname)==-1)	return "";
 	var begin=nowurl.indexOf(paraname)+paraname.length+1;
