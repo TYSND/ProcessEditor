ca14b1706dcfa69135fec525d761cb3ba72fc9a7
LPJworkroom <lpjworkroom@gmail.com>
2019-11-18 16:47:11 +0800

    publicLeft.php navItem all is clickable

--- php/publicLeft.php
+++ php/publicLeft.php
@@ -5,9 +5,9 @@
 		<div id="navigatorBar">
 			<h2 style="margin-left:10%;">导航</h2>
 			<div class="" style="width:100%">
-				<div class="navBarItem"><a href="myApply.html">我的申请</a></div>
-				<div class="navBarItem"><a href="myCheck.html">我的审核</a></div>
-				<div class="navBarItem"><a href="myProcess.html">我的流程</a></div>
+				<a href="myApply.html"><div class="navBarItem">我的申请</div></a>
+				<a href="myCheck.html"><div class="navBarItem">我的审批</div></a>
+				<a href="myProcess.html"><div class="navBarItem">我的流程</div></a>
 			</div>
 		</div>
 		<button class="submitBut" onclick="location.href=\'php/quit.php\'" style="width:100%;margin:5px 0 5px 0">
